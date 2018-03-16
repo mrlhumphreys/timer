897e28dce79fd64440f20d090fb7147f11e3335a
Mark Humphreys <mark@mrlhumphreys.com>
2018-03-16 22:32:46 +1100

    add time recording

--- src/application.js
+++ src/application.js
@@ -7,25 +7,63 @@ const INTERVAL = 10;
 
 var timerState = STOPPED;
 var currentTimeMs = 0;
+var times = [];
+
+var splitTime = function(timestamp) {
+  let milliseconds = timestamp % 1000;
+  let seconds = Math.floor(timestamp / 1000) % 60;
+  let minutes = Math.floor(timestamp / 60000);
+  return { minutes, seconds, milliseconds };
+};
+
+var formatTimeComponents = function(components) {
+  let milliseconds = String(components.milliseconds).padStart(3, '0').substr(0,2);
+  let seconds = String(components.seconds).padStart(2, '0');
+  let minutes = String(components.minutes).padStart(2, '0');
+  return { minutes, seconds, milliseconds };
+};
+
+var joinTimeComponents = function(components) {
+  return components.minutes + ':' + components.seconds + '.' + components.milliseconds;
+};
+
+var formatTime = function(timestamp) {
+  return joinTimeComponents(formatTimeComponents(splitTime(timestamp)));
+};
 
 var tick = function() {
   currentTimeMs = currentTimeMs + INTERVAL; 
-  let millisecondsPart = String(currentTimeMs % 1000).padStart(3,'0').substr(0, 2);
-  let secondsPart = Math.floor(currentTimeMs / 1000);
-  document.getElementById('time').innerHTML = secondsPart + ':' + millisecondsPart; 
+  document.getElementById('time').innerHTML = formatTime(currentTimeMs); 
 };
 
 var ticker = function() { }; 
 
+var addTime = function(time) {
+  times.push(time);
+
+  let lis = times.map(function(e) {
+    return "<li>" + formatTime(e) + "</li>";
+  }).reverse().join('');
+
+  document.getElementById('times_list').innerHTML = lis;
+
+  let sum = times.reduce(function(a,b) { return a + b; });
+  let avg = Math.round(sum / times.length);
+
+  document.getElementById('average_time').innerHTML = formatTime(avg);
+};
+
 var ready = function() {
   timerState = READY;
   currentTimeMs = 0;
+  document.getElementById('time').innerHTML = formatTime(currentTimeMs);; 
   document.getElementById('time').classList.add('ready');
 };
 
 var stop = function() {
   timerState = STOPPED;
   clearInterval(ticker);
+  addTime(currentTimeMs);
   document.getElementById('time').classList.remove('running');
 };
 
@@ -37,9 +75,7 @@ var run = function() {
 };
 
 var handleKeyDown = function(event) {
-  let keyName = event.key;
-
-  if (keyName === SPACEBAR) {
+  if (event.key === SPACEBAR) {
     if (timerState === STOPPED) {
       ready();
     } else if (timerState === RUNNING) {
@@ -49,14 +85,24 @@ var handleKeyDown = function(event) {
 };
 
 var handleKeyUp = function(event) {
-  let keyName = event.key;
-
-  if (keyName === SPACEBAR) {
+  if (event.key === SPACEBAR) {
     if (timerState === READY) {
       run();
     }
   }
 };
 
+var handleClick = function(event) {
+  if (event.target.id === 'sidebar_toggle') {
+    document.getElementById('wrapper').classList.toggle('show_sidebar');
+    if (event.target.innerHTML === "&gt;&gt;") {
+      event.target.innerHTML = "&lt;&lt;";
+    } else {
+      event.target.innerHTML = "&gt;&gt;";
+    }
+  }
+};
+
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
+document.addEventListener("click", handleClick);
